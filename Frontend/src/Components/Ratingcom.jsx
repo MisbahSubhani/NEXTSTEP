166aa58e52879f8a0cd01ceaa4df4f93a758b026
subhani <ms7411@srmist.edu.in>
2025-01-30 12:25:33 +0530

    Change the Colour

--- Frontend/src/Components/Ratingcom.jsx
+++ Frontend/src/Components/Ratingcom.jsx
@@ -2,9 +2,9 @@ import { Typography, Avatar, Rating } from "@material-tailwind/react";
  
 export function RatingWithComment() {
   return (
-    <div  className="flex flex-row  bg-zinc-100 w-full flex flex-row justify-between pt-4  "
+    <div  className="flex flex-row    w-full flex flex-row justify-between pt-4  "
      >
-    <div className="px-8 text-center border-t-2"  >
+    <div className="px-8 text-center "  >
    
       <Avatar
         src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
@@ -19,7 +19,7 @@ export function RatingWithComment() {
       </Typography>
       <Rating value={4} readonly />
     </div>
-    <div className="px-8 text-center p border-t-2">
+    <div className="px-8 text-center p ">
    
    <Avatar
      src="https://plus.unsplash.com/premium_photo-1671656349218-5218444643d8?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YXZhdGFyfGVufDB8fDB8fHww"
@@ -34,7 +34,7 @@ export function RatingWithComment() {
    </Typography>
    <Rating value={5} readonly />
  </div>
- <div className=" lg:px-8 text-center pt-4 border-t-2" >
+ <div className=" lg:px-8 text-center pt-4 " >
    
    <Avatar
      src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8YXZhdGFyfGVufDB8fDB8fHww"
