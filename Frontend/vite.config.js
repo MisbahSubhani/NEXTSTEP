59ff27690fca6070dda5e91a71c6ed3e59f68bc6
subhani <ms7411@srmist.edu.in>
2025-04-19 21:21:43 +0530

    vercel  deploy

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'frontend/build', // Ensures build files go to 'frontend/build'
-  },
-  base: '/', // Ensure base URL is set to root, so assets load correctly
-});
+  base: "/",
+})
