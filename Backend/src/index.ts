cec87054dc6666de9a983332bd983244da52ccb9
subhani <ms7411@srmist.edu.in>
2025-04-19 20:35:46 +0530

    vercel  deploy

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,21 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import authRoutes from './routes/authRoutes';
+import hrRoutes from './routes/hrRoutes';
+import studentRoutes from './routes/studentRoutes';
+import profileRoutes from './routes/profileRoutes';
+import feedbackRoutes from './routes/feedbackRoutes';
+import internshipRoutes from './routes/internshipRoutes';
+import applicationRoutes from './routes/applicationRoutes';
+import noticeRoutes from './routes/noticeRoutes';
+import { VercelRequest, VercelResponse } from '@vercel/node';
 
-import express from "express";
-import cors from "cors";
-import authRoutes from "./routes/authRoutes";
-import hrRoutes from "./routes/hrRoutes";
-import studentRoutes from "./routes/studentRoutes";
-import profileRoutes from "./routes/profileRoutes";
-import feedbackRoutes from "./routes/feedbackRoutes";
-import internshipRoutes from "./routes/internshipRoutes";
-import applicationRoutes from "./routes/applicationRoutes"
-import noticeRoutes from "./routes/noticeRoutes"
-
+// Create Express app
 const app = express();
 
+// Middleware
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors());
 
+// Routes
 app.use(authRoutes);
 app.use(hrRoutes);
 app.use(studentRoutes);
@@ -25,4 +28,7 @@ app.use(internshipRoutes);
 app.use(applicationRoutes);
 app.use(noticeRoutes);
 
-export default app;
+// Export the Express app as the Vercel serverless function
+export default (req: VercelRequest, res: VercelResponse) => {
+  app(req, res);
+};
